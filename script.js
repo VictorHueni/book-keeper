ae9d507e0c30df933df9c5e1838a205d15e13dc5
Victor Hüni <victor.hueni@protonmail.com>
2021-07-18 17:46:14 +0200

    store bookmarks in localStorage

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
 // Show Modal, Focus on Input
-function showModal(){
+function showModal() {
     modal.classList.add('show-modal');
     websiteNameEl.focus();
 }
@@ -15,40 +17,68 @@ function showModal(){
 // Modal Event Listeners
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
-window.addEventListener('click', (e) => e.target === modal  ? modal.classList.remove('show-modal') : false);
+window.addEventListener('click', (e) => e.target === modal ? modal.classList.remove('show-modal') : false);
 
 // Validate Form
-function validate(nameValue, urlValue){
+function validate(nameValue, urlValue) {
     const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g
     const regex = new RegExp(expression);
-    if(!nameValue || !urlValue){
+    if (!nameValue || !urlValue) {
         alert('Please submit values for both fields.');
         return false;
     }
 
-    if(urlValue.match(regex)) {
+    if (urlValue.match(regex)) {
         return true;
     }
 
-    if(!urlValue.match(RegExp)) {
+    if (!urlValue.match(RegExp)) {
         alert('PLease provide a valid web address.');
         return false
     }
 }
 
+// Fetch Bookmarks from localStorage
+function fetchBookmarks() {
+    // Get Bookmarks from localStorage if available
+    if (localStorage.getItem('bookmarks')) {
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else {
+        //Create bookmarks array in localStorage
+        bookmarks = [{
+            name: 'Zero To mastery',
+            url: 'zerotomastery.io/',
+        }];
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+}
+
 // Handle Data from Form
-function storeBookmark(e){
+function storeBookmark(e) {
     e.preventDefault();
     const nameValue = websiteNameEl.value;
     let urlValue = websiteUrlEl.value;
     if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
         urlValue = `https://${urlValue}`;
     }
-    if(!validate(nameValue, urlValue)){
+    if (!validate(nameValue, urlValue)) {
         return false;
     }
 
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+
+    bookmarkForm.reset();
+    websiteNameEl.focus();
 }
 
 // Event Listener
 bookmarkForm.addEventListener('submit', storeBookmark);
+
+// On load Fetch bookmarks
+fetchBookmarks();
